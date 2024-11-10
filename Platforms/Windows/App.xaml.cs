39973d3d5f0b060d3ed779e8504c6f8e7f561fd6
Tie C <ty.dev@outlook.com>
2024-11-09 23:17:37 -0800

    Remove dark mode

--- Platforms/Windows/App.xaml.cs
+++ Platforms/Windows/App.xaml.cs
@@ -17,6 +17,7 @@ public partial class App : MauiWinUIApplication
     public App()
     {
         this.InitializeComponent();
+        RequestedTheme = ApplicationTheme.Light;
     }
 
     protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
