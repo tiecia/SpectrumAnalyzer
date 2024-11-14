d727e6fb68e0a3aff8f62b0ca89c99c8ea05200b
Tie C <ty.cia@outlook.com>
2024-11-13 18:34:54 -0800

    change default settings

--- Settings.cs
+++ Settings.cs
@@ -3,7 +3,7 @@
 public class Settings
 {
     public int FftBins { get; set; } = 512;
-    public bool BinFft { get; set; } = true;
+    public bool BinFft { get; set; } = false;
     public bool Normalize { get; set; } = true;
     public bool NegativeFrequencies { get; set; } = false;
 }
